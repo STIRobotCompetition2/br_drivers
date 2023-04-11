54780e02a05462666f42316e2a781437dc457164
Sven Becker <sven.becker@epfl.ch>
2023-04-12 00:07:52 +0200

    Remove legacy code

--- src/raspberry_camera_v2_driver.cpp
+++ src/raspberry_camera_v2_driver.cpp
@@ -92,49 +92,6 @@ class CameraDriver : public rclcpp::Node {
         else RCLCPP_DEBUG(this->get_logger(), "Driver sent picture no picture although loop was executed (no pub-flag true)");
 
     }
-    void run(){
-        rclcpp::Rate video_rate(10);
-        cv::Mat cv_image;
-        
-        cv_bridge::CvImage cv_bridge_image;
-        cv_bridge_image.encoding = "bgr8";
-        sensor_msgs::msg::Image ros_image_raw;
-        sensor_msgs::msg::CompressedImage ros_image_compressed;
-        std_msgs::msg::Header header;
-        header.frame_id = "camera_sensor_link";
-
-
-        while(cam.getVideoFrame(cv_image,10000) && rclcpp::ok()){
-            if(publish_raw || publish_compressed){
-                cv_bridge_image.image = cv_image;
-                header.stamp = this->get_clock()->now();
-            }
-            if(publish_raw){
-                cv_bridge_image.toImageMsg(ros_image_raw);
-                ros_image_raw.encoding = sensor_msgs::image_encodings::BGR8;
-                ros_image_raw.header = header;
-
-                image_raw_pub->publish(ros_image_raw);
-            }
-            if(publish_compressed){
-                cv_bridge_image.toCompressedImageMsg(ros_image_compressed, cv_bridge::JPEG);
-                ros_image_compressed.format = static_cast<std::string>("jpeg");
-                ros_image_compressed.header = header;
-
-                
-                image_compressed_pub->publish(ros_image_compressed);
-            }
-
-            if(publish_raw || publish_compressed) RCLCPP_DEBUG(this->get_logger(), "Driver sent picture");
-            else RCLCPP_DEBUG(this->get_logger(), "Driver sent picture no picture although loop was executed (no pub-flag true)");
-            video_rate.sleep();
-
-        }
-        RCLCPP_WARN(this->get_logger(), "CAM DONE");
-        cam.stopVideo();
-
-
-    }
     lccv::PiCamera cam;
     double rate;
     bool publish_raw;
