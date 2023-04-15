0bcec625f0516bf17ed1b2fabe350c6694171bfb
Sven Becker <sven.becker@epfl.ch>
2023-04-15 15:49:55 +0200

    Add warp-transform for bird-eye view based on aruco grid as well

--- src/raspberry_camera_v2_driver.cpp
+++ src/raspberry_camera_v2_driver.cpp
@@ -90,6 +90,7 @@ class CameraDriver : public rclcpp::Node {
             return;
         }
         if(publish_raw || publish_compressed){
+            if(warp_available) cv::warpPerspective(cv_image_raw, cv_image_raw, warp_transform, cv::Size(cam.options->video_width, cam.options->video_height));
             cv::resize(cv_image_raw, cv_image_ds, cv::Size(ros_resolution_width,ros_resolution_height), cv::INTER_LINEAR);
             cv_bridge_image.image = cv_image_ds;
             header.stamp = this->get_clock()->now();
@@ -132,48 +133,107 @@ class CameraDriver : public rclcpp::Node {
         cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_250);
         cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
 
-        cv_image_raw = cv::imread("/home/budget_roomba_2/sven_space/ros_ws/src/camera_calib.jpg", cv::IMREAD_COLOR);
+        // cv_image_raw = cv::imread("/home/budget_roomba_2/sven_space/ros_ws/src/camera_calib.jpg", cv::IMREAD_COLOR);
         cv::aruco::detectMarkers(cv_image_raw, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
-        
-        Eigen::MatrixXd Y, X1, X2;;
-        Y.resize(markerIds.size() * 4, 2);
-        X1.resize(markerIds.size() * 4, 2);
-        X2.resize(markerIds.size() * 4, 2);
-        X1.col(1).fill(1);
-        X2.col(1).fill(1);
-
-        // Construct Dataset
-        double y_offset_mm = -1 * (request->number_in_y - 1.) / 2. * request->spacing_y_mm;
+        std::array<cv::Point2f, 4u> border_points, dst_points;
         for(size_t id = 0; id < markerIds.size(); id++){
-            Eigen::Vector2d center;
-            center.x() = request->x_offset_mm + static_cast<double>(markerIds.at(id) / static_cast<size_t>(request->number_in_y)) * request->spacing_x_mm;
-            center.y() = -1 * (y_offset_mm + static_cast<double>(markerIds.at(id) % static_cast<size_t>(request->number_in_y)) * request->spacing_y_mm);
-            std::cerr << "ID " << markerIds.at(id) << " CTR: " << center.transpose() << std::endl << std::endl;
-            for(size_t c = 0; c < 4; c++){
-                Y.row(c + id * 4) = center + Eigen::Vector2d(1,0) * (c < 2 ? 1 : -1) * request->corner_dist_mm / 2.0 + Eigen::Vector2d(0,1) * (c % 3 == 0 ? 1 : -1) * request->corner_dist_mm / 2.0;
-                std::cerr << "\t\t<<>> " << c << " CRNR GT: " << Y.row(c + id * 4) << std::endl;
-                X1(c + id * 4, 0) = markerCorners.at(id).at(c).y;
-                X2(c + id * 4, 0) = markerCorners.at(id).at(c).x;
-                std::cerr << "\t\t<<>> " << c << " CRNR DT: " << X1(c + id * 4, 0) << " " << X2(c + id * 4, 0) << std::endl;
+            if(markerIds.at(id) == 0) 
+                border_points.at(0) = markerCorners.at(id).at(3);
+            if(markerIds.at(id) == request->number_in_y - 1) 
+                border_points.at(1) = markerCorners.at(id).at(2);
+            if(markerIds.at(id) == request->number_in_y * (request->number_in_x - 1)) 
+                border_points.at(2) = markerCorners.at(id).at(0);
+            if(markerIds.at(id) == request->number_in_y * request->number_in_x - 1) 
+                border_points.at(3) = markerCorners.at(id).at(1);
+        }
+        // size_t image_center_x = cv_image_raw.size().width / 2;
+        // size_t image_center_y = cv_image_raw.size().height / 2;
 
-            }
+        dst_points.at(0).x = std::min(border_points.at(0).x, border_points.at(2).x);
+        dst_points.at(2).x = dst_points.at(0).x;
+        dst_points.at(1).x = std::max(border_points.at(1).x, border_points.at(3).x);
+        dst_points.at(3).x = dst_points.at(1).x;
+
+        dst_points.at(0).y = std::max(border_points.at(0).y, border_points.at(1).y);
+        dst_points.at(1).y = dst_points.at(0).y;
+        dst_points.at(2).y = std::min(border_points.at(2).y, border_points.at(3).y);
+        dst_points.at(3).y = dst_points.at(2).y;
+        // Inverted Logic (1 step in x-direction of pattern = 1 step in y-direction of image)
+        double ratio = static_cast<double>(request->number_in_y * request->spacing_y_mm + request->corner_dist_mm) / static_cast<double>(request->number_in_x * request->spacing_x_mm + request->corner_dist_mm);
+        size_t delta_x = dst_points.at(1).x - dst_points.at(0).x;
+        size_t delta_y = dst_points.at(0).y - dst_points.at(2).y;
+        if(delta_x > ratio * delta_y){
+            delta_x = static_cast<size_t>(ratio * delta_y);
+            size_t midpoint = (dst_points.at(1).x + dst_points.at(0).x) / 2;
+            dst_points.at(0).x = midpoint - delta_x / 2;
+            dst_points.at(2).x = midpoint - delta_x / 2;
+            dst_points.at(1).x = midpoint + delta_x / 2;
+            dst_points.at(3).x = midpoint + delta_x / 2;
+        }
+        else{
+            delta_y = static_cast<size_t>(delta_x / ratio);
+            size_t midpoint = (dst_points.at(0).y + dst_points.at(2).y) / 2;
+            dst_points.at(0).y = midpoint + delta_y / 2;
+            dst_points.at(1).y = midpoint + delta_y / 2;
+            dst_points.at(2).y = midpoint - delta_y / 2;
+            dst_points.at(3).y = midpoint - delta_y / 2;
         }
 
+        delta_x = dst_points.at(1).x - dst_points.at(0).x;
+        delta_y = dst_points.at(0).y - dst_points.at(2).y;
+        std::cerr << "dx: " << delta_x << std::endl;
+        std::cerr << "dy: " << delta_y << std::endl;
+        std::cerr << "ra: " << ratio << std::endl;
 
-        // Perform Linear Regression
-        Eigen::Vector2d sol_x = (X1.transpose() * X1).inverse() * X1.transpose() * Y.col(0);
-        Eigen::Vector2d sol_y = (X2.transpose() * X2).inverse() * X2.transpose() * Y.col(1);
+
+
+
+        warp_transform = cv::getPerspectiveTransform(border_points, dst_points);
+        warp_available = true;
+        std::cerr << "W T: " << warp_transform << std::endl;
+
+
+
+        // // Construct Dataset
+        // Eigen::MatrixXd Y, X1, X2;;
+        // Y.resize(markerIds.size() * 4, 2);
+        // X1.resize(markerIds.size() * 4, 2);
+        // X2.resize(markerIds.size() * 4, 2);
+        // X1.col(1).fill(1);
+        // X2.col(1).fill(1);
+
         
-        std::cerr << "SOLX: " << sol_x.transpose() << std::endl;
-        std::cerr << "SOLY: " << sol_y.transpose() << std::endl;
-        // std::cerr << X1 << std::endl;
-        // std::cerr << Y << std::endl;
-        std::cerr << "X ex: " << X1.block<10,2>(0,0).transpose() << std::endl;
-        std::cerr << "SOLX ex: " << sol_x.transpose() * X1.block<10,2>(0,0).transpose() << std::endl;
-        std::cerr << "GTX " << Y.col(0).block<10,1>(0,0) << std::endl;
+        // double y_offset_mm = -1 * (request->number_in_y - 1.) / 2. * request->spacing_y_mm;
+        // for(size_t id = 0; id < markerIds.size(); id++){
+        //     Eigen::Vector2d center;
+        //     center.x() = request->x_offset_mm + static_cast<double>(markerIds.at(id) / static_cast<size_t>(request->number_in_y)) * request->spacing_x_mm;
+        //     center.y() = -1 * (y_offset_mm + static_cast<double>(markerIds.at(id) % static_cast<size_t>(request->number_in_y)) * request->spacing_y_mm);
+        //     std::cerr << "ID " << markerIds.at(id) << " CTR: " << center.transpose() << std::endl << std::endl;
+        //     for(size_t c = 0; c < 4; c++){
+        //         Y.row(c + id * 4) = center + Eigen::Vector2d(1,0) * (c < 2 ? 1 : -1) * request->corner_dist_mm / 2.0 + Eigen::Vector2d(0,1) * (c % 3 == 0 ? 1 : -1) * request->corner_dist_mm / 2.0;
+        //         std::cerr << "\t\t<<>> " << c << " CRNR GT: " << Y.row(c + id * 4) << std::endl;
+        //         X1(c + id * 4, 0) = markerCorners.at(id).at(c).y;
+        //         X2(c + id * 4, 0) = markerCorners.at(id).at(c).x;
+        //         std::cerr << "\t\t<<>> " << c << " CRNR DT: " << X1(c + id * 4, 0) << " " << X2(c + id * 4, 0) << std::endl;
 
-        std::cerr << "SOLY ex: " << sol_y.transpose() * X2.block<10,2>(0,0).transpose() << std::endl;
-        std::cerr << "GTY " << Y.col(1).block<10,1>(0,0) << std::endl;
+        //     }
+        // }
+
+
+        // // Perform Linear Regression
+        // Eigen::Vector2d sol_x = (X1.transpose() * X1).inverse() * X1.transpose() * Y.col(0);
+        // Eigen::Vector2d sol_y = (X2.transpose() * X2).inverse() * X2.transpose() * Y.col(1);
+        
+        // std::cerr << "SOLX: " << sol_x.transpose() << std::endl;
+        // std::cerr << "SOLY: " << sol_y.transpose() << std::endl;
+        // // std::cerr << X1 << std::endl;
+        // // std::cerr << Y << std::endl;
+        // std::cerr << "X ex: " << X1.block<10,2>(0,0).transpose() << std::endl;
+        // std::cerr << "SOLX ex: " << sol_x.transpose() * X1.block<10,2>(0,0).transpose() << std::endl;
+        // std::cerr << "GTX " << Y.col(0).block<10,1>(0,0) << std::endl;
+
+        // std::cerr << "SOLY ex: " << sol_y.transpose() * X2.block<10,2>(0,0).transpose() << std::endl;
+        // std::cerr << "GTY " << Y.col(1).block<10,1>(0,0) << std::endl;
 
 
 
@@ -186,6 +246,9 @@ class CameraDriver : public rclcpp::Node {
     bool publish_raw;
     bool publish_compressed;
     cv::Mat cv_image_raw, cv_image_ds;
+
+    cv::Mat warp_transform;
+    bool warp_available = false;
         
     cv_bridge::CvImage cv_bridge_image;
     sensor_msgs::msg::Image ros_image_raw;
